6d231ebd17a255aa1bc6b723131a86307b4c8580
GoldenEdenIV <trandoanductai@gmail.com>
2025-12-26 16:56:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ console.log(">>> server.js loaded");
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "elysian.goldeneden.io.vn" })); // FE Vite
+app.use(cors({ origin: "https://elysian.goldeneden.io.vn" })); // FE Vite
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
